77fc30cb740df6c9e3e79c58106d8cda12010c73
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2022-06-25 15:31:20 +0200

    Update with ssl certificate.

--- myslurm.sh
+++ myslurm.sh
@@ -5,38 +5,47 @@
 
 set -e
 
+# You may change this one to yours
 MYSLURM_ROOT=${HOME}/install_mn/slurm
 
 MYSLURM_BIN_DIR=${MYSLURM_ROOT}/bin
 MYSLURM_SBIN_DIR=${MYSLURM_ROOT}/sbin
-MYSLURM_CONF_DIR=${MYSLURM_ROOT}/slurm-confdir
-MYSLURM_VAR_DIR=${MYSLURM_ROOT}/var
 
+MYSLURM_CONF_DIR=${MYSLURM_ROOT}/slurm-confdir
 MYSLURM_CONF_FILE=${MYSLURM_CONF_DIR}/slurm.conf
 
-# Cleanup
-rm -rf ${MYSLURM_VAR_DIR}/slurm*
-> ${MYSLURM_VAR_DIR}/accounting   # clear the file.
-mkdir ${MYSLURM_VAR_DIR}/slurmd
+MYSLURM_VAR_DIR=${MYSLURM_CONF_DIR}/var
 
-# Gather system
+# Cleanup and var regeneration
+rm -rf ${MYSLURM_VAR_DIR}/slurm*
+mkdir -p ${MYSLURM_VAR_DIR}/slurmd ${MYSLURM_VAR_DIR}/slurmctld
+echo "" > ${MYSLURM_VAR_DIR}/accounting   # clear the file.
+
+# Generate key (only once)
+[ -f myslurm.crt ] ||
+	openssl req -x509 -sha256 -days 3650 -newkey rsa -config myslurm.cnf -keyout myslurm.key -out myslurm.crt
+[ -f ${MYSLURM_CONF_DIR}/slurm.crt ] || cp myslurm.crt ${MYSLURM_CONF_DIR}/slurm.crt
+[ -f ${MYSLURM_CONF_DIR}/slurm.key ] || cp myslurm.key ${MYSLURM_CONF_DIR}/slurm.key
+
+# Get system info: nodes (local and remote), cores, sockets, cpus, memory
 NODELIST=$(scontrol show hostname | paste -d" " -s)
 
 MASTER_NODE=$(hostname)                    # Master node
 
 REMOTE_NODES_LIST=(${NODELIST/"${MASTER_NODE}"})  # List of remote nodes (removing master)
-REMOTE_NODES_STR=${REMOTE_NODES_LIST[*]}               # "node1 node2 node3"
+REMOTE_NODES_STR=${REMOTE_NODES_LIST[*]}          # "node1 node2 node3"
 REMOTE_NODES_STR=${REMOTE_NODES_STR// /,}         # "node1,node2,node3"
 REMOTE_NODES_COUNT=${#REMOTE_NODES_LIST[@]}
+
+((REMOTE_NODES_COUNT == 0)) && echo "Error: REMOTE_NODES_COUNT is zero" >&2
 
 NSOCS=$(grep "physical id" /proc/cpuinfo | sort -u | wc -l) # Number of CPUS (sockets)
 NCPS=$(grep -c "physical id[[:space:]]\+: 0" /proc/cpuinfo) # cores per socket
 MEMORY=$(grep MemTotal /proc/meminfo | cut -d' ' -f8)       # memory in KB
 
-# Create MYSLURM_CONF_FILE ===========================================
-
-{
-	sed -e "s|@MYSLURM_VAR_DIR@|${MYSLURM_VAR_DIR}|g" slurm.conf.base
+{   # Scope to redirect output
+	sed -e "s|@MYSLURM_VAR_DIR@|${MYSLURM_VAR_DIR}|g" \
+		-e "s|@MYSLURM_CONF_DIR@|${MYSLURM_CONF_DIR}|g" myslurm.conf.base
 
 	echo "DefMemPerNode=$((MEMORY / 1024))"
 	echo "SlurmctldHost=${MASTER_NODE}"
@@ -51,16 +60,16 @@ MEMORY=$(grep MemTotal /proc/meminfo | cut -d' ' -f8)       # memory in KB
 mpiexec -n ${REMOTE_NODES_COUNT} --hosts=${REMOTE_NODES_STR} hostname | sed -e "s/^/# Remote: /"
 
 # Start the server and clients =======================================
-echo "HEE ${MYSLURM_SBIN_DIR}/slurmctld"
-echo "HOO ${MYSLURM_SBIN_DIR}/slurmd"
 ${MYSLURM_SBIN_DIR}/slurmctld -cDvf ${MYSLURM_CONF_FILE} &
-mpiexec -n ${REMOTE_NODES_COUNT} --hosts=${REMOTE_NODES_STR} ${MYSLURM_SBIN_DIR}/slurmd -cDvf ${MYSLURM_CONF_FILE} &
+mpiexec -n ${REMOTE_NODES_COUNT} \
+		--hosts=${REMOTE_NODES_STR} \
+		${MYSLURM_SBIN_DIR}/slurmd -cDvf ${MYSLURM_CONF_FILE} &
 
 # echo "# From inside"
-$MYSLURM_BIN_DIR/sinfo
-$MYSLURM_BIN_DIR/sbatch -JMyTestJob -N2 ./HEREYOURJOB.sh &
-$MYSLURM_BIN_DIR/squeue
-$MYSLURM_BIN_DIR/sinfo
+${MYSLURM_BIN_DIR}/sinfo
+${MYSLURM_BIN_DIR}/sbatch -JMyTestJob -N2 ./HEREYOURJOB.sh &
+${MYSLURM_BIN_DIR}/squeue
+${MYSLURM_BIN_DIR}/sinfo
 
 aux=$(${MYSLURM_BIN_DIR}/squeue | wc -l);
 
@@ -71,5 +80,5 @@ while [ $aux -gt 1 ]; do
 done
 
 echo "Finishing...";
-$MYSLURM_BIN/sacct
+${MYSLURM_BIN_DIR}/sacct
 
