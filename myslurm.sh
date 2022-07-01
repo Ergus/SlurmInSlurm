b70b31baac773ea23db54e73cba2c75627dc552c
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2022-07-01 04:09:15 +0200

    Finish previous changes

--- myslurm.sh
+++ myslurm.sh
@@ -8,14 +8,21 @@ if [[ $0 == ${BASH_SOURCE[0]} ]]; then
     exit 1
 fi
 
-# Hardcoded directories ==============================================
+# Hardcoded info ==============================================
+MARIADB_ROOT=${HOME}/install_mn/mariadb
+MARIADB_PORT=7100
+
 MUNGE_ROOT=${HOME}/install_mn/munge
 MUNGE_STATEDIR=/tmp/munge
+
 # You may change this one to yours
 MYSLURM_ROOT=${HOME}/install_mn/slurm
+MYSLURM_USER=bsc28860
+MYSLURM_DBD_PORT=7101
 
 # Get system info ====================================================
 MYSLURM_MASTER=$(hostname)                     # Master node
+MYSLURM_IP=$(hostname -i)                      # Master ip
 NODELIST=$(scontrol show hostname | paste -d" " -s)
 
 REMOTE_LIST=(${NODELIST/"${MYSLURM_MASTER}"})  # List of remote nodes (removing master)
@@ -29,11 +36,7 @@ if ((MYSLURM_NSLAVES == 0)); then
 	return 1
 fi
 
-NSOCS=$(grep "physical id" /proc/cpuinfo | sort -u | wc -l) # Number of CPUS (sockets)
-NCPS=$(grep -c "physical id[[:space:]]\+: 0" /proc/cpuinfo) # cores per socket
-MEMORY=$(grep MemTotal /proc/meminfo | cut -d' ' -f8)       # memory in KB
-
-# Create and clean directories =======================================
+# Create and clean directories and database ==========================
 MYSLURM_CONF_DIR=${MYSLURM_ROOT}/slurm-confdir
 [[ -d "${MYSLURM_CONF_DIR}" ]] || mkdir ${MYSLURM_CONF_DIR}
 
@@ -50,22 +53,63 @@ for node in ${REMOTE_LIST[@]}; do
 	mkdir ${MYSLURM_VAR_DIR}/slurmd.${node}
 done
 
+# Database ===========================================================
+MARIADB_DATA=${MYSLURM_VAR_DIR}/mariadb-data
+rm -rf ${MARIADB_DATA} && mkdir ${MARIADB_DATA}
+rm -rf ${MYSQL_UNIX_PORT}
+
+# Create storage
+mysql_install_db --verbose \
+				 --datadir=${MARIADB_DATA} \
+				 --basedir=${MARIADB_ROOT} \
+				 --user=${MYSLURM_USER} \
+				 --port=${MARIADB_PORT}
+
+# Start server
+mysqld_safe --verbose \
+			--datadir=${MARIADB_DATA} \
+			--basedir=${MYSLURM_ROOT} \
+			--user=${MYSLURM_USER} \
+			--port=${MARIADB_PORT} &
+
+# Start server
+while ! [[ -S ${MYSQL_UNIX_PORT} ]] ; do
+  echo "Waiting mysql socket: ${MYSQL_UNIX_PORT}"
+  sleep 1
+done
+echo "Waiting mysql done"
+
+MARIADB_CMD="GRANT ALL ON slurm_acct_db.* TO '${MYSLURM_USER}'@'localhost' WITH GRANT OPTION;"
+MARIADB_CMD="GRANT ALL ON slurm_acct_db.* TO '${MYSLURM_USER}'@'localhost' WITH GRANT OPTION;"
+MARIADB_CMD+="CREATE DATABASE slurm_acct_db;"
+MARIADB_CMD+="FLUSH PRIVILEGES;"
+
+mysql -u ${MYSLURM_USER} --port=${MARIADB_PORT} --protocol=SOCKET \
+	  --execute="${MARIADB_CMD}"
+
 # Generate files =====================================================
 
-MYSLURM_CONF_FILE=${MYSLURM_CONF_DIR}/slurm.conf
-{ # Create MYSLURM_CONF_FILE
-	sed -e "s|@MYSLURM_VAR_DIR@|${MYSLURM_VAR_DIR}|g" \
-		-e "s|@MYSLURM_CONF_DIR@|${MYSLURM_CONF_DIR}|g" myslurm.conf.base
+# Create MYSLURM_CONF_FILE
+NODENAMES=$(scontrol show hostlistsorted ${MYSLURM_SLAVES})
 
-	echo "SlurmctldHost=${MYSLURM_MASTER}"
+NSOCS=$(grep "physical id" /proc/cpuinfo | sort -u | wc -l) # Number of CPUS (sockets)
+NCPS=$(grep -c "physical id[[:space:]]\+: 0" /proc/cpuinfo) # cores per socket
 
-	REALMEM=$(((MEMORY / 1024 / 1024) * 1024))
-	NODENAMES=$(scontrol show hostlistsorted ${MYSLURM_SLAVES})
+MEMORY=$(grep MemTotal /proc/meminfo | cut -d' ' -f8)       # memory in KB
+REALMEM=$(((MEMORY / 1024 / 1024) * 1024))
 
-	echo "NodeName=${NODENAMES} Sockets=${NSOCS} CoresPerSocket=${NCPS} ThreadsPerCore=1 MemSpecLimit=4000 RealMemory=${REALMEM}"
-	echo "PartitionName=malleability Nodes=ALL Default=YES MaxTime=INFINITE State=UP"
-	echo ""
-} > ${MYSLURM_CONF_FILE}
+sed -e "s|@MYSLURM_VAR_DIR@|${MYSLURM_VAR_DIR}|g" \
+	-e "s|@MYSLURM_CONF_DIR@|${MYSLURM_CONF_DIR}|g" \
+	-e "s|@MYSLURM_MASTER@|${MYSLURM_MASTER}|g" \
+	-e "s|@MYSLURM_USER@|${MYSLURM_USER}|g" \
+	-e "s|@MYSLURM_DBD_PORT@|${MYSLURM_DBD_PORT}|g" \
+	-e "s|@REALMEM@|${REALMEM}|g" \
+	-e "s|@NODENAMES@|${NODENAMES}|g" \
+	-e "s|@NSOCS@|${NSOCS}|g" \
+	-e "s|@MYSLURM_IP@|${MYSLURM_IP}|g" \
+	-e "s|@NCPS@|${NCPS}|g" \
+	slurm.conf.base > ${MYSLURM_CONF_DIR}/slurm.conf
+
 
 # Copy the lua lmod
 [[ -f ${MYSLURM_VAR_DIR}/myslurm/personal.lua ]] ||
@@ -90,12 +134,26 @@ MYSLURM_CONF_FILE=${MYSLURM_CONF_DIR}/slurm.conf
 
 } > ${MYSLURM_CONF_DIR}/topology.conf
 
+sed -e "s|@MYSLURM_VAR_DIR@|${MYSLURM_VAR_DIR}|g" \
+	-e "s|@MYSLURM_MASTER@|${MYSLURM_MASTER}|g" \
+	-e "s|@MYSLURM_USER@|${MYSLURM_USER}|g" \
+	-e "s|@MYSLURM_DBD_PORT@|${MYSLURM_DBD_PORT}|g" \
+	-e "s|@MARIADB_PORT@|${MARIADB_PORT}|g" \
+	slurmdbd.conf.base > ${MYSLURM_CONF_DIR}/slurmdbd.conf
+
+chmod 600 ${MYSLURM_CONF_DIR}/slurmdbd.conf
+
 # Wrapper
 sed -e "s|@MUNGE_STATEDIR@|${MUNGE_STATEDIR}|g" \
 	-e "s|@MUNGE_ROOT@|${MUNGE_ROOT}|g" \
 	-e "s|@MYSLURM_MASTER@|${MYSLURM_MASTER}|g" \
+	-e "s|@MYSLURM_USER@|${MYSLURM_USER}|g" \
+	-e "s|@MYSLURM_DBD_PORT@|${MYSLURM_DBD_PORT}|g" \
 	-e "s|@MYSLURM_ROOT@|${MYSLURM_ROOT}|g" \
-	-e "s|@MYSLURM_CONF_FILE@|${MYSLURM_CONF_FILE}|g" \
+	-e "s|@MYSLURM_CONF_DIR@|${MYSLURM_CONF_DIR}|g" \
+	-e "s|@MARIADB_ROOT@|${MARIADB_ROOT}|g" \
+	-e "s|@MARIADB_DATA@|${MARIADB_DATA}|g" \
+	-e "s|@MARIADB_PORT@|${MARIADB_PORT}|g" \
 	mywrapper.sh.base > ${MYSLURM_CONF_DIR}/mywrapper.sh
 
 chmod a+x ${MYSLURM_CONF_DIR}/mywrapper.sh
@@ -114,7 +172,6 @@ mpiexec -n $((MYSLURM_NSLAVES + 1)) --hosts=${NODELIST// /,} ${MYSLURM_CONF_DIR}
 # Exports environment at the end to avoid modifying the environment
 export MYSLURM_ROOT=${MYSLURM_ROOT}
 export MYSLURM_CONF_DIR=${MYSLURM_CONF_DIR}
-export MYSLURM_CONF_FILE=${MYSLURM_CONF_FILE}
 export MYSLURM_VAR_DIR=${MYSLURM_VAR_DIR}
 
 export MYSLURM_MASTER=${MYSLURM_MASTER}
@@ -125,7 +182,7 @@ export MYSLURM_NSLAVES=${MYSLURM_NSLAVES}      # number of slaves
 	export MODULEPATH=${MYSLURM_VAR_DIR}:${MODULEPATH}
 
 myslurm () {
-	SLURM_CONF=${MYSLURM_CONF_FILE} ${MYSLURM_ROOT}/bin/$@
+	SLURM_CONF=${MYSLURM_CONF_DIR}/slurm.conf ${MYSLURM_ROOT}/bin/$@
 }
 export -f myslurm
 # # echo "# From inside"
@@ -134,3 +191,6 @@ myslurm sinfo
 myslurm squeue
 
 module load myslurm
+
+#create account 
+sacctmgr create user name=${MYSLURM_USER=bsc28860} DefaultAccount=root AdminLevel=Admin
